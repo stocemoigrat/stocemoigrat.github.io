0d134c7398680d61e425891de42e2e1ab98d359e
stocemoigrat <124303879+stocemoigrat@users.noreply.github.com>
2023-03-01 20:34:26 +0100

    update

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ games[games.length] = {
 	bestPlayerCount: [5],
 	coop: false,
 	type: "hidden roles",
-	tags: ["hidden roles", "traitor", "team-based", "thematic", "asymmetric"],
+	tags: ["hidden roles", "traitor", "thematic", "asymmetric"],
 	expansions: [],
 	description: "<i>The year is 1913. The steamship SS Atlantica has set out on its voyage across the Atlantic Ocean. Its unsuspecting passengers are fully anticipating a calm journey. However, tensions rise when a body is discovered in the ship's chapel, signs of a strange ritual littered around the corpse.</i><br><br>Some players are humans who are fighting for the survival of the ship, while others are traitors sent aboard the ship by the Deep Ones to ensure that it never reaches port! Because player loyalties are hidden, determining who is friend and who is foe is critical to winning a thrilling game of Unfathomable."
 };
@@ -207,6 +207,198 @@ games[games.length] = {
 	expansions: [],
 	description: ""
 };
+games[games.length] = {
+	name: "Scythe",
+	imageName: "scythe.jpg", imagePosition: "",
+	bggLink: "https://boardgamegeek.com/boardgame/169786/scythe",
+	weight: 4,
+	timeToPlay: "2.5h", timeToPlayNum: 150,
+	timeToLearn: "40", timeToLearnNum: 40,
+	playerCount: [1, 2, 3, 4, 5, 6, 7],
+	recPlayerCount: [1, 2, 3, 4, 5, 6, 7],
+	bestPlayerCount: [4],
+	coop: false,
+	type: "strategy",
+	tags: ["strategy", "engine-building", "asymmetric", "conflict"],
+	expansions: ["Invaders from Afar", "Rise of Fenris", "Modular board"],
+	description: ""
+};
+games[games.length] = {
+	name: "Set",
+	imageName: "set.jpg", imagePosition: "background-position-y: center;",
+	bggLink: "https://boardgamegeek.com/boardgame/1198/set",
+	weight: 1,
+	timeToPlay: "15", timeToPlayNum: 15,
+	timeToLearn: "5", timeToLearnNum: 5,
+	playerCount: [2, 3, 4, 5, 6, 7, 8],
+	recPlayerCount: [2, 3, 4, 5, 6, 7, 8],
+	bestPlayerCount: [3, 4],
+	coop: false,
+	type: "party",
+	tags: ["party", "pattern recognition", "real-time"],
+	expansions: [],
+	description: "The best game."
+};
+games[games.length] = {
+	name: "The Shipwreck Arcana",
+	imageName: "shipwreckarcana.jpg", imagePosition: "",
+	bggLink: "https://boardgamegeek.com/boardgame/220517/shipwreck-arcana",
+	weight: 1,
+	timeToPlay: "30", timeToPlayNum: 30,
+	timeToLearn: "10", timeToLearnNum: 10,
+	playerCount: [2, 3, 4, 5],
+	recPlayerCount: [2, 3, 4, 5],
+	bestPlayerCount: [3, 4],
+	coop: true,
+	type: "deduction",
+	tags: ["deduction", "math", "limited communication"],
+	expansions: [],
+	description: ""
+};
+games[games.length] = {
+	name: "Spirit Island",
+	imageName: "spiritisland.jpg", imagePosition: "",
+	bggLink: "https://boardgamegeek.com/boardgame/162886/spirit-island",
+	weight: 3.5,
+	timeToPlay: "2.5h", timeToPlayNum: 150,
+	timeToLearn: "30", timeToLearnNum: 30,
+	playerCount: [1, 2, 3, 4, 5, 6],
+	recPlayerCount: [1, 2, 3, 4],
+	bestPlayerCount: [2, 3],
+	coop: true,
+	type: "strategy",
+	tags: ["strategy", "area control", "asymmetric"],
+	expansions: ["Jagged Earth", "Branch & Claw", "Feather & Flame"],
+	description: ""
+};
+games[games.length] = {
+	name: "Through the Ages: A New Story of Civilization",
+	imageName: "throughtheages.jpg", imagePosition: "",
+	bggLink: "https://boardgamegeek.com/boardgame/182028/through-ages-new-story-civilization",
+	weight: 5,
+	timeToPlay: "5h+", timeToPlayNum: 301,
+	timeToLearn: "1h", timeToLearnNum: 60,
+	playerCount: [2, 3, 4],
+	recPlayerCount: [2, 3, 4],
+	bestPlayerCount: [3],
+	coop: false,
+	type: "strategy",
+	tags: ["strategy", "engine building", "conflict"],
+	expansions: [],
+	description: ""
+};
+games[games.length] = {
+	name: "Trickerion",
+	imageName: "trickerion.jpg", imagePosition: "",
+	bggLink: "https://boardgamegeek.com/boardgame/255668/trickerion-collectors-edition",
+	weight: 5,
+	timeToPlay: "3h", timeToPlayNum: 180,
+	timeToLearn: "1h", timeToLearnNum: 60,
+	playerCount: [1, 2, 3, 4],
+	recPlayerCount: [1, 2, 3, 4],
+	bestPlayerCount: [4],
+	coop: false,
+	type: "strategy",
+	tags: ["strategy", "worker placement"],
+	expansions: ["Dawn of Technology", "Dahlgaard's Gifts", "Dahlgaard's Academy"],
+	description: ""
+};
+games[games.length] = {
+	name: "The Crew: Mission Deep Sea",
+	imageName: "thecrew.jpg", imagePosition: "",
+	bggLink: "https://boardgamegeek.com/boardgame/324856/crew-mission-deep-sea",
+	weight: 1.5,
+	timeToPlay: "10", timeToPlayNum: 10,
+	timeToLearn: "10", timeToLearnNum: 10,
+	playerCount: [2, 3, 4, 5],
+	recPlayerCount: [2, 3, 4, 5],
+	bestPlayerCount: [4],
+	coop: true,
+	type: "trick-taking",
+	tags: ["trick-taking", "limited communication"],
+	expansions: [],
+	description: ""
+};
+games[games.length] = {
+	name: "Rococo",
+	imageName: "rococo.jpg", imagePosition: "",
+	bggLink: "https://boardgamegeek.com/boardgame/296100/rococo-deluxe-edition",
+	weight: 3,
+	timeToPlay: "2h", timeToPlayNum: 120,
+	timeToLearn: "15", timeToLearnNum: 15,
+	playerCount: [1, 2, 3, 4, 5],
+	recPlayerCount: [1, 2, 3, 4, 5],
+	bestPlayerCount: [3],
+	coop: false,
+	type: "strategy",
+	tags: ["strategy", "set collection", "deck-building", "area majority"],
+	expansions: ["Jewelry Box"],
+	description: ""
+};
+games[games.length] = {
+	name: "Res Arcana",
+	imageName: "resarcana.jpg", imagePosition: "",
+	bggLink: "https://boardgamegeek.com/boardgame/262712/res-arcana",
+	weight: 2,
+	timeToPlay: "45", timeToPlayNum: 45,
+	timeToLearn: "15", timeToLearnNum: 15,
+	playerCount: [2, 3, 4, 5],
+	recPlayerCount: [2, 3, 4, 5],
+	bestPlayerCount: [2],
+	coop: false,
+	type: "strategy",
+	tags: ["strategy", "combo building", "race"],
+	expansions: ["Lux et Tenebrae"],
+	description: "Choose your mage, gather essences, craft unique artifacts, and use them to summon dragons, conquer places of power, and achieve victory!"
+};
+games[games.length] = {
+	name: "Sagrada",
+	imageName: "sagrada.jpg", imagePosition: "",
+	bggLink: "https://boardgamegeek.com/boardgame/199561/sagrada",
+	weight: 1.5,
+	timeToPlay: "45", timeToPlayNum: 45,
+	timeToLearn: "10", timeToLearnNum: 10,
+	playerCount: [1, 2, 3, 4],
+	recPlayerCount: [1, 2, 3, 4],
+	bestPlayerCount: [2],
+	coop: false,
+	type: "abstract",
+	tags: ["abstract", "open drafting", "pattern building"],
+	expansions: [],
+	description: "Build the most beautiful stained glass window by drafting dice and placing them on your player board. Each board has some restrictions on which color or value of dice can be placed there. While the game starts off with many possibilities, later rounds will prove to be a real brain burner!"
+};
+games[games.length] = {
+	name: "Deception: Murder in Hong Kong",
+	imageName: "deception.jpg", imagePosition: "",
+	bggLink: "https://boardgamegeek.com/boardgame/156129/deception-murder-hong-kong",
+	weight: 1,
+	timeToPlay: "30", timeToPlayNum: 30,
+	timeToLearn: "10", timeToLearnNum: 10,
+	playerCount: [4, 5, 6, 7, 8, 9, 10, 11, 12],
+	recPlayerCount: [5, 6, 7, 8, 9, 10, 11, 12],
+	bestPlayerCount: [6, 7, 8],
+	coop: false,
+	type: "hidden roles",
+	tags: ["hidden roles", "traitor", "deduction"],
+	expansions: [],
+	description: "Players take on the roles of investigators attempting to solve a murder case – but there's a twist. The killer is one of the investigators! Each player's role and team are randomly assigned at the start of play and include the unique roles of Forensic Scientist, Witness, Investigator, Murderer, and Accomplice. While the Investigators attempt to deduce the truth, the murderer's team must deceive and mislead.<br><br>The Forensic Scientist has the solution but can only give clues while the investigators (and the murderer) attempt to interpret the evidence. In order to succeed, the investigators must not only deduce the truth from the clues of the Forensic Scientist, they must also see through the misdirection being injected into the equation by the Murderer and Accomplice!"
+};
+games[games.length] = {
+	name: "Similo",
+	imageName: "similo.jpg", imagePosition: "",
+	bggLink: "https://boardgamegeek.com/boardgame/268620/similo",
+	weight: 1,
+	timeToPlay: "15", timeToPlayNum: 15,
+	timeToLearn: "5", timeToLearnNum: 5,
+	playerCount: [2, 3, 4, 5, 6, 7, 8],
+	recPlayerCount: [2, 3, 4, 5, 6, 7, 8],
+	bestPlayerCount: [3, 4],
+	coop: true,
+	type: "deduction",
+	tags: ["deduction", "limited communication"],
+	expansions: ["Animals", "Fables"],
+	description: "You have 5 rounds to let the other players find the secret character among the twelve shown. Play more characters as clues, to suggest similarities or differences with the secret character. After each clue, the other players must discard 1 or more characters from the roster. If they discard the secret character, you all lose! But, if the secret character is the only one left, you all win!"
+};
 
 var collection = document.getElementById("collection");
 games.forEach(addGame);
@@ -286,7 +478,7 @@ function closeWindow() {
 	infoWindow.style.display = 'none';
 }
 
-window.onclick = function(event) {
+document.querySelector('body').onclick = function(event) {
   if (event.target == infoWindow) {
     infoWindow.style.display = 'none';
   }
